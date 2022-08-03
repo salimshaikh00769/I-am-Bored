b916ee47391ce9ad82745a55d92be1cb56550ff8
Salim shaikh <salimshaikh00769@gmail.com>
2022-08-03 20:33:46 +0530

    issue resolve

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,32 +7,25 @@ import "../index.css"
 
 function Home() {
   let box;
-    const [acti, setActivity] = useState('');
+    const [activity, setActivity] = useState('');
       fetch("https://www.boredapi.com/api/activity")
     .then(res => res.json())
     .then(act => box = act);
 
-   const [mood, setMood] = useState('../component/Bored.jpg')
-
-
     const counter = () => {
       setActivity(box.activity)
-      console.log(box.activity)
-      setMood("http://clipart-library.com/images/ziX5raEXT.jpg")
-      console.log(mood)
     }
 
-    let path = acti==='' ? Bored : happy ;
-    let heading = acti==='' ? "Are you Bored ?" : "Do this"
-    let button = acti==='' ? "Click me for fun" : "Still Bored- Click again"
+    let path = activity==='' ? Bored : happy ;
+    let heading = activity==='' ? "Are you Bored ?" : "Do this"
+    let button = activity==='' ? "Click me for fun" : "Still Bored- Click again"
    
   return (
     <div className='container'>
-        <h1>{heading} {acti}</h1>
+        <h1>{heading} {activity}</h1>
         <img src={path} alt="bored" height={'200px'} />
         <br />
         <button onClick={counter}>{button}</button>
-        <h1></h1>
     </div>
   )
 }
